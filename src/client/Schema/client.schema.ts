82ec64762032e450c1be32695b799cf72a93c686
DevWCastle <pedrocastelobrancodosantos@gmail.com>
2025-06-13 16:12:59 -0300

    Criando e exportando o ClientSchema que define as propriedades do Client

---
+++ src/client/Schema/client.schema.ts
@@ -0,0 +1,29 @@
+
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+
+
+export type ClientDocument = HydratedDocument<Client> //esta criando um tipo, que é uma referencia a esta classe Client.
+
+@Schema()//Define que a classe abaixo sera um Schema
+export class Client extends Document{//cria o Schema extendendo do Documents que é do TS
+
+    @Prop({required: true})//Define as propriedades da classe Client, e tem como parametros alguns metodos de verificação.
+    name: string
+
+    @Prop({required: true})
+    cpf: number
+
+    @Prop({required: true})
+    email: string
+
+    @Prop({ required: true})
+    password: string
+
+    @Prop({ required: true})
+    createdData: Date
+
+}
+
+
+export const ClientSchema = SchemaFactory.createForClass(Client)//Tranforma a classe client em um schema utilizando o SchemaFactory

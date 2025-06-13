f813c097ac10304c140d256ca9a3bff476925942
DevWCastle <pedrocastelobrancodosantos@gmail.com>
2025-06-13 17:13:18 -0300

    Criando o controller client

--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateClientService } from './Services/create-client.service';
+import { IClientEntity } from './Interfaces/IClientEntity';
 
 @Controller('client')
-export class ClientController {}
+export class ClientController {
+
+    constructor(
+        private readonly createClientService: CreateClientService
+    ){}
+
+    @Post("create")// http://api.com/client/create no metodo post 
+    async create(@Body() client: IClientEntity): Promise<IClientEntity>{
+    //precisa de um body com um client do tipo ICleintEntity como parametros e retorna uma promise desse mesmo tipo
+
+    const newClient =  this.createClientService.execute(client)
+    return newClient
+
+    }
+
+
+}

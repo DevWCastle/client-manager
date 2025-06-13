ee88941cae39a5f71d002f4e783641415eadaf7e
DevWCastle <pedrocastelobrancodosantos@gmail.com>
2025-06-13 16:50:32 -0300

    criando a lógica de execução do createClientService

--- src/client/Services/create-client.service.ts
+++ src/client/Services/create-client.service.ts
@@ -1,7 +1,23 @@
+import { Injectable } from "@nestjs/common";
+import { CreateClientRepository } from "../Repositories/create-client.repository";
+import { IClientEntity } from "../Interfaces/IClientEntity";
+
+@Injectable()
+
+export class CreateClientService{
+    constructor(
+        private readonly createClientRepository: CreateClientRepository
+    ){}
 
 
+    async execute(client: IClientEntity): Promise<string>{//poderia trocar esse retorno para uma httpResponse no caso ou sla
+        const newClient = await this.createClientRepository.execute(client)
+        
+        return newClient.name
 
+    }
 
+}
 
 
 

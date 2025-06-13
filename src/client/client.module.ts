46847b22c148f85718b8528f7c7b68bd22dfd997
DevWCastle <pedrocastelobrancodosantos@gmail.com>
2025-06-13 16:51:23 -0300

    adicionando os providers do Client Module

--- src/client/client.module.ts
+++ src/client/client.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ClientController } from './client.controller';
+import { CreateClientRepository } from './Repositories/create-client.repository';
+import { CreateClientService } from './Services/create-client.service';
 
 @Module({
-  controllers: [ClientController]
+  controllers: [ClientController],
+  providers: [CreateClientRepository, CreateClientService],
+  exports: [CreateClientRepository, CreateClientService]
 })
 export class ClientModule {}

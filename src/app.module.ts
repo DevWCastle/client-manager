238fe915d7eefe7d97c1a1c4ccf885418e598e4b
DevWCastle <pedrocastelobrancodosantos@gmail.com>
2025-06-13 17:12:58 -0300

    importando o MongooseModulePara conctar no mongo database atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientModule } from './client/client.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ClientModule],
+  imports: [
+    ClientModule, 
+    MongooseModule.forRoot('mongodb+srv://DevWCastle:<db_password>@client-manager.1qrmbgp.mongodb.net/?retryWrites=true&w=majority&appName=client-manager')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

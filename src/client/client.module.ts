0dc949e6d901c95b7227d3b925267a36692f19a0
DevWCastle <pedrocastelobrancodosantos@gmail.com>
2025-06-13 17:13:38 -0300

    configurando o mongooseModule no client module

--- src/client/client.module.ts
+++ src/client/client.module.ts
@@ -2,8 +2,17 @@ import { Module } from '@nestjs/common';
 import { ClientController } from './client.controller';
 import { CreateClientRepository } from './Repositories/create-client.repository';
 import { CreateClientService } from './Services/create-client.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Client, ClientSchema } from './Schema/client.schema';
 
 @Module({
+  imports:[
+    MongooseModule.forFeature([{
+      name: Client.name,
+      schema: ClientSchema
+    }])
+  ],
+
   controllers: [ClientController],
   providers: [CreateClientRepository, CreateClientService],
   exports: [CreateClientRepository, CreateClientService]

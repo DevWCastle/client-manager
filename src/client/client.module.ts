4f2c4021147df7921bcf7904c71a1e8f01232f73
DevWCastle <pedrocastelobrancodosantos@gmail.com>
2025-06-14 14:36:25 -0300

    adicionando os providers necessarios para funcionalidade do metodo update do client

--- src/client/client.module.ts
+++ src/client/client.module.ts
@@ -8,6 +8,8 @@ import { GetClientRepository } from './Repositories/get-client.repositoty';
 import { GetClientService } from './Services/get-client.service';
 import { DeleteClientRepository } from './Repositories/delete-client.repository';
 import { DeleteClientService } from './Services/delete-client.service';
+import { UpdateClientRepository } from './Repositories/update-client.repository';
+import { UpdateClientService } from './Services/update-client.service';
 
 @Module({
   imports:[
@@ -21,12 +23,14 @@ import { DeleteClientService } from './Services/delete-client.service';
   providers: [
     CreateClientRepository, CreateClientService, 
     GetClientRepository, GetClientService, 
-    DeleteClientRepository, DeleteClientService
+    DeleteClientRepository, DeleteClientService,
+    UpdateClientRepository, UpdateClientService
   ],
   exports: [
     CreateClientRepository, CreateClientService,
     GetClientRepository, GetClientService,
-    DeleteClientRepository, DeleteClientService
+    DeleteClientRepository, DeleteClientService,
+    UpdateClientRepository, UpdateClientService
   ]
 })
 export class ClientModule {}

aa126a3d8764bb30375172ef18ae579815c0de15
DevWCastle <pedrocastelobrancodosantos@gmail.com>
2025-06-14 13:10:35 -0300

    adicionando o metodo delete no controller

--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -1,14 +1,18 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { CreateClientService } from './Services/create-client.service';
 import { IClientEntity } from './Interfaces/IClientEntity';
 import { GetClientService } from './Services/get-client.service';
+import { DeleteClientService } from './Services/delete-client.service';
+import { response } from 'express';
+import { DeleteResult } from 'mongoose';
 
 @Controller('client')
 export class ClientController {
 
     constructor(
         private readonly createClientService: CreateClientService,
-        private readonly getClientService: GetClientService
+        private readonly getClientService: GetClientService,
+        private readonly deleteClientService: DeleteClientService
     ){}
 
     @Post("create")// http://api.com/client/create no metodo post 
@@ -20,11 +24,18 @@ export class ClientController {
 
     }
 
-    @Get("clients")
+    @Get()
     async getClients(): Promise<IClientEntity[]>{
         const clients =  await this.getClientService.execute()
 
         return clients;
+    }
+
+    @Delete('delete/:id')
+    async delete(@Param('id') idClient: string): Promise<DeleteResult>{
+
+        const deleteCount = await this.deleteClientService.execute(idClient);
+        return deleteCount;
     }
 
 
